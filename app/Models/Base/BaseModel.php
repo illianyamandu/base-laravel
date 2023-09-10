8cc4fad5c799583ed26b66bf24cb331a1132237a
Illian Yamandú <yamandu.developer@gmail.com>
2023-09-10 17:10:46 -0300

    BASE-1: fixing UUID trait name

--- app/Models/Base/BaseModel.php
+++ app/Models/Base/BaseModel.php
@@ -2,12 +2,12 @@
 
 namespace App\Models\Base;
 
-use App\Traits\UUID;
+use App\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class BaseModel extends Model
 {
     use HasFactory;
-    use UUID;
+    use Uuid;
 }
