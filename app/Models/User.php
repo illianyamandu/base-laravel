03bd7abbe46494d4b9d26891b72bcdd0a91eedfd
Illian Yamandú <yamandu.developer@gmail.com>
2023-09-10 16:51:27 -0300

    BASE-1: alt

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,9 +2,8 @@
 
 namespace App\Models;
 
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use App\Traits\UUID;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
