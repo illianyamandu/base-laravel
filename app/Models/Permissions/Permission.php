3f9fee55442660df49f025da9754894383dbd0fd
Illian Yamandú <yamandu.developer@gmail.com>
2023-09-18 23:07:27 -0300

    BASE-15: check if permission exists before valide if user has permission

--- app/Models/Permissions/Permission.php
+++ app/Models/Permissions/Permission.php
@@ -4,6 +4,7 @@ namespace App\Models\Permissions;
 
 use App\Models\Base\BaseModel;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Support\Str;
 
 class Permission extends BaseModel
 {
@@ -25,5 +26,21 @@ class Permission extends BaseModel
     public function groups(): BelongsToMany
     {
         return $this->belongsToMany(Group::class, 'group_permission');
+    }
+
+    /**
+     * @param string $val
+     */
+    public static function getPermissionByVal(string $val): ?Permission
+    {
+        $permission = self::query();
+
+        if (Str::isUuid($val)) {
+            $permission = $permission->where('id', $val);
+        } else {
+            $permission = $permission->where('identifier_name', $val);
+        }
+
+        return $permission->first();
     }
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Models\Permissions\Permission;
 use App\Models\User;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
@@ -21,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::before(fn (User $user, $ability) => $user->hasPermissionTo($ability));
+        Gate::before(function (User $user, $ability) {
+            if (Permission::getPermissionByVal($ability)) {
+                return $user->hasPermissionTo($ability);
+            }
+        });
     }
 }
